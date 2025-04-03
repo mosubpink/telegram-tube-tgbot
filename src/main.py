9567cef97925c8931e74bb56d5582b317a744f41
blank <mosubpink@proton.me>
2025-04-03 08:28:34 +0200

    update

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
+import os
 import logging
 
 from environs import env
-from telegram import Update
+from yt_dlp import YoutubeDL
+from telegram import Update, Video
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
     ContextTypes,
     MessageHandler,
-    filters,
+    filters
 )
+import yt_dlp
 
 
 logging.basicConfig(
@@ -17,20 +20,29 @@ logging.basicConfig(
 )
 
 env.read_env()
-api_id  = env('id')
-api_hash = env('hash')
 token = env('token')
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 async def yt_download(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:
-    pass
+    url = update.message.text
+    try:
+        with YoutubeDL() as dl:
+            info_dict = dl.extract_info(url, download=True)
+            video_file = dl.prepare_filename(info_dict)
+            
+            await update.message.reply_video(video_file)
+            
+            os.remove(video_file)
+    except  Exception as Error:
+            print(Error)
 
 bot = ApplicationBuilder().token(token).build()
 
 bot.add_handler(CommandHandler("start", hello))
+bot.add_handler(MessageHandler(filters.Entity("url") ,yt_download))
 
 
 bot.run_polling()
