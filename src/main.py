cd7e165fea3b6be93b08877bff616e1e3bbb6ef5
blank <mosubpink@proton.me>
2026-01-20 10:13:14 +0200

    style: change style to PEP 8 conventions

--- src/main.py
+++ src/main.py
@@ -13,34 +13,34 @@ from telegram.ext import (
 )
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level = logging.INFO
 )
 
 env.read_env()
-token = env('token')
+token: str = env('token')
 
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-async def yt_download(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:
-    url = update.message.text
+async def yt_download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url: str = update.message.text
     try:
         with YoutubeDL() as dl:
-            info_dict = dl.extract_info(url, download=True)
-            video_file = dl.prepare_filename(info_dict)
+            info_dict: str = dl.extract_info(url, download=True)
+            video_file: None = dl.prepare_filename(info_dict)
             
             await update.message.reply_video(video_file)
             
             os.remove(video_file)
-    except  Exception as Error:
+    except Exception as Error:
             print(Error)
 
 bot = ApplicationBuilder().token(token).build()
 
 bot.add_handler(CommandHandler("start", hello))
-bot.add_handler(MessageHandler(filters.Entity("url") ,yt_download))
+bot.add_handler(MessageHandler(filters.Entity("url"), yt_download))
 
 
 bot.run_polling()
